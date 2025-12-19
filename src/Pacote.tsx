de9f2e44bda3ce0c8547f25ca9f0d7ce83350afd
Gianlucca Paiva <gianluccapaiva@gmail.com>
2025-12-18 21:34:01 -0300

    adiciona componente Pacote

---
+++ src/Pacote.tsx
@@ -0,0 +1,17 @@
+import "./Pacote.css"
+export default function Pacote(){
+    return (
+        <div className="pacote">
+                <dt>b88e3490-2ffd-44c7-af0e-00a4cb70b53d</dt>
+                <dd></dd>
+                <dt>Largura (cm)</dt>
+                <dd>10</dd>
+                <dt>Porfundidade (cm)</dt>
+                <dd>20</dd>
+                <dt>Altura (cm)</dt>
+                <dd>10</dd>
+                <dt>Peso (kg)</dt>
+                <dd>10</dd>
+        </div>
+    )
+}
